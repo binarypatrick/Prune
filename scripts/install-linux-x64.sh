b099f32cb1c31819e51722e9e7dc0328ebb053c1
Patrick M <3362020+BinaryPatrick@users.noreply.github.com>
2023-07-03 23:38:15 -0400

    docs: add install script

---
+++ scripts/install-linux-x64.sh
@@ -0,0 +1,8 @@
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/binarypatrick/prune/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_FILE="prune-linux-x64.tar.gz"
+GITHUB_URL="https://github.com/BinaryPatrick/Prune/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+
+curl -L -o prune-linux-x64.tar.gz $GITHUB_URL
+tar xzvf prune-linux-x64.tar.gz ./prune
+install -Dm 755 prune -t /usr/local/bin
+rm prune prune-linux-x64.tar.gz
