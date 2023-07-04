6b69582aaf3a169f1654f4e41cf254e3e3888c63
Patrick M <3362020+BinaryPatrick@users.noreply.github.com>
2023-07-03 23:46:35 -0400

    docs: add additional script

---
+++ scripts/install-linux-arm64.sh
@@ -0,0 +1,8 @@
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/binarypatrick/prune/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_FILE="prune-linux-arm64.tar.gz"
+GITHUB_URL="https://github.com/BinaryPatrick/Prune/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+
+curl -s -L -o prune-linux-arm64.tar.gz $GITHUB_URL
+tar xzf prune-linux-arm64.tar.gz ./prune
+install -Dm 755 prune -t /usr/local/bin
+rm prune prune-linux-arm64.tar.gz
